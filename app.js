3ca5789f1a1ab3a8fa104e6d5e62ea59c4db5a2d
Cody Brown <cody6lefever@gmail.com>
2020-02-19 11:32:36 -0800

    cors

--- app.js
+++ app.js
@@ -1,39 +1,59 @@
 const express = require('express');
 const data = require('./geo.js');
-// const ?something? = require('./darksky.json');
+const weather = require('./darksky.js');
+const cors = require('cors');
 const app = express();
 // const request = require('superagent');
 
+app.use(cors());
 // localhost:9000/ returns 'Hello World!'
 // request not using yet
 // respond with respond.send
 app.get('/', (request, respond) => respond.send('Hello World!'));
 
+// initialize the global state of lat and lng so it's accessible in other routes
+let lat;
+let lng;
+
 // localhost:9000/location (the data/cityData) returns
-//request using /location
+// request using /location
 // setting cityData to data.results 0 index from our data js file
 app.get('/location', (request, respond) => {
+    const location = request.query.search;
+
     const cityData = data.results[0];
+
+    // update the global state of lat and lng so it's accessible in other routes
+    lat = cityData.geometry.location.lat;
+    lng = cityData.geometry.location.lng;
 
     //respond with the data from js file 
     respond.json({
-        // ??????????????????
         formatted_query: cityData.formatted_address,
         latitude: cityData.geometry.location.lat,
         longitude: cityData.geometry.location.lng,
     });
 });
 
+const getWeatherData = (lat, lng) => {
+    return weather.daily.data.map(forcast => {
+        return {
+            forecast: forecast.summary,
+            time: new Date(forecast.time * 1000),
+        };
+    })
+};
 
-// app.get('/weather', (request, respond) => {
-//     const weatherData = ?something?.results[0];
-// })
+// localhost:9000/weather
+app.get('/weather', (req, res) => {
+    const portlandWeather = getWeatherData(lat, lng);
+
+    res.json(portlandWeather);
+});
 
 // if no result 404
 app.get('*', (req, res) => res.json('404!!!'));
 
-// using port/localhost 9000
-const port = process.envPORT || 9000;
-
-app.listen(port, () => console.log (`Example app listening on port ${port}`));
-
+module.exports = {
+    app: app,
+};
