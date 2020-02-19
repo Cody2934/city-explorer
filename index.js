f592426ee530fc00a13c8b43cdfbf946321707ce
Cody Brown <cody6lefever@gmail.com>
2020-02-19 11:39:34 -0800

    time to deploy

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const { app } = require('./app.js');
-const port = process.envPORT || 9000;
+const port = process.env.PORT || 9000;
 
 app.listen(port, () => console.log (`Example app listening on port ${port}`));
