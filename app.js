a6758bfb214b4e604a4093f280ba7d73080300ff
Cody Brown <cody6lefever@gmail.com>
2020-02-20 10:38:05 -0800

    more bugfix

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.get('/reviews', async(req, res, next) => {
 app.get('/hiking', async(req, res, next) => {
     try {
         const trail = await request.get(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lng}&maxDistance=10&key=${process.env.HIKING_API_KEY}`)
-        const hikingStuff = trail.hiking.map(trail => {
+        const hikingStuff = trail.body.trails.map(trail => {
             return {
                 name: trail.name,
                 location: trail.length,
